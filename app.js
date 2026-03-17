3b7dfa32e0a2e54d02b773dcef7630b1c0d27856
JKaren2025 <240097@utxicoepec.edu.mx>
2026-03-17 11:52:23 -0600

    Backend_Tienda_Virtual
    Se crearon nuevas librerias

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const logger = require('morgan');
 const bodyParser = require('body-parser');
 
 const http = require('http');
+const path = require('path');
 const app = express();
 
 app.use(logger('dev'));//para crear archivos en el sistema, en el console
@@ -13,7 +14,7 @@ app.get('/', (req, res) =>  res.status(200).send({
     message: 'Bienvenido a mi APIde Tienda Virtual',
 }));
 
-const port = parseInt(process.env.PORT) || 3000;//puerto de react 51.30  a 51,34 ES PARA QUE NO CHOQUE
+const port = parseInt(process.env.PORT) || 8000;//puerto de react 51.30  a 51,34 ES PARA QUE NO CHOQUE
 app.set('port', port);
 
 const server = http.createServer(app);
